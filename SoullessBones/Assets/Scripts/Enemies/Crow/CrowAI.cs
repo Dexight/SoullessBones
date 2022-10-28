92f3e20d6c3397f43cf2f28b789d2313c41a5042
Dexight <92735754+Dexight@users.noreply.github.com>
2022-10-28 22:15:18 +0300

    Crow
    Add crow enemy

---
+++ SoullessBones/Assets/Scripts/Enemies/Crow/CrowAI.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+
+public class CrowAI : MonoBehaviour
+{
+    #region Variables
+    private Transform _transform;
+    private Transform _player;
+    private Collider2D[] _playerCollider;
+    private Rigidbody2D _rigidbody;
+    private BoxCollider2D _collider;
+
+    [Header("Movement")]
+    public float _normalSpeed = 4f;
+    public float _slowlySpeed = 2f;
+    public float _AttackDistance;
+    public float _SlowlyDistance;
+    [SerializeField] private CompositeCollider2D groundCollider;
+
+    [Header("Attack")]
+    public int damage;
+    public float timeBtwDamage;
+
+    [Header("Conditions")]
+    [SerializeField] private bool facingRight;
+    private bool Slowly = false;
+    #endregion
+
+    void Start()
+    {   
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _playerCollider = _player.GetComponents<Collider2D>();
+        _transform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
+
+        groundCollider = GameObject.FindGameObjectWithTag("Ground").GetComponent<CompositeCollider2D>();
+
+        Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), groundCollider); //игнорирование столкновения с землёй
+        _rigidbody.gravityScale = 0; // обнуляем гравитацию после взлёта
+    }
+
+    private void Update()
+    {
+        Slowly = Vector2.Distance(_player.position, _transform.position) < _SlowlyDistance;
+        Flip();
+        if (_collider.IsTouching(_playerCollider[0]) || _collider.IsTouching(_playerCollider[1]) || _collider.IsTouching(_playerCollider[2]))
+        {
+            FindObjectOfType<HealthSystem>().TakeDamage(damage);
+        }
+    }
+
+    void FixedUpdate()
+   {
+        //замедляется при приближении к игроку
+        if (!Slowly)
+            NormalAttack();
+        else
+            SlowlyAttack();
+    }
+
+    private void NormalAttack()
+    {
+        _transform.position = Vector2.MoveTowards(_transform.position, _player.position, _normalSpeed * Time.fixedDeltaTime);
+    }
+
+    private void SlowlyAttack()
+    {
+        _transform.position = Vector2.MoveTowards(_transform.position, _player.position, _slowlySpeed * Time.fixedDeltaTime);
+    }
+
+    private void Flip()
+    {
+        if (_player.position.x < _transform.position.x && facingRight)
+        {
+            facingRight = !facingRight;
+            FlipCur();
+        }
+        else if (_player.position.x > _transform.position.x && !facingRight)
+        {
+            facingRight = !facingRight;
+            FlipCur();
+        }
+    }
+
+    void FlipCur()
+    {
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+ 
+    //private void OnTriggerStay2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        FindObjectOfType<HealthSystem>().TakeDamage(damage);
+    //    }
+    //}
+}
---
+++ SoullessBones/Assets/Scripts/Enemies/Crow/IdleCrow.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleCrow : MonoBehaviour
+{
+    #region Variables
+    private CrowAI CrowAI;
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private Transform _player;
+    [SerializeField] private float _upSpeed;
+    private bool Attacking = false;
+
+    [Header("Attack Trigger Box")]
+    [SerializeField] private BoxCollider2D aggroCollider;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private Vector2 _AttackBoxPosition;
+    [SerializeField] private Vector2 aggroSize;
+    #endregion
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _transform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        CrowAI = GetComponent<CrowAI>();
+
+        _rigidbody.gravityScale = 1;
+        CrowAI.enabled = false; //отключаем скрипт "полёта"
+    }
+
+    void Update()
+    {
+        if (PlayerInSight()) //триггер при попадании игрока в поле зрения (нужно будет дополнить ещё триггер при получении урона или физического столкновения)
+        {
+            Attacking = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(Attacking)
+            StartCoroutine(GetUpCorutine()); //метод полёта (в конце удаляет этот скрипт)
+    }
+
+    private IEnumerator GetUpCorutine() 
+    {
+        yield return new WaitForSeconds(0.2f);
+        _animator.SetTrigger("toFly");
+        Vector3 Up = new Vector3(0f, 3f);
+        transform.position = Vector2.MoveTowards(transform.position, transform.position + Up, _upSpeed * Time.fixedDeltaTime);
+        yield return new WaitForSeconds(0.2f);
+        CrowAI.enabled = true;
+        Destroy(_transform.GetComponent<IdleCrow>());
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(aggroCollider.bounds.center + transform.right * _AttackBoxPosition.x * transform.localScale.x + transform.up * _AttackBoxPosition.y, aggroCollider.bounds.size * aggroSize,
+            0, Vector2.right, 0, playerLayer);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(aggroCollider.bounds.center + transform.right * _AttackBoxPosition.x * transform.localScale.x + transform.up * _AttackBoxPosition.y, aggroCollider.bounds.size * aggroSize);
+    }
+}
