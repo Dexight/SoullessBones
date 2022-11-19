51232708dc7a0d14358fa7fd59483753740732cb
Dexight <92735754+Dexight@users.noreply.github.com>
2022-11-20 00:56:33 +0300

    Some levels fixes

--- SoullessBones/Assets/ParallaxLayer.cs
+++ SoullessBones/Assets/ParallaxLayer.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class ParallaxLayer : MonoBehaviour
 {
     Transform m_target;
-    [SerializeField] float m_YScrollSpeed;
-    [SerializeField] float m_XScrollSpeed;
+    [SerializeField] float m_YScrollSpeed; //скорость скрола по оси У
+    [SerializeField] float m_XScrollSpeed;//скорость скрола по оси Х
+    public Vector2 startPosition;
     private void Start()
     {
         m_target = Camera.main.transform;
     }
     private void Update()
     {
-        transform.position = new Vector2(m_target.position.x * m_XScrollSpeed, m_target.position.y * m_YScrollSpeed);
+        transform.position = new Vector2((startPosition.x + m_target.position.x) * m_XScrollSpeed,(startPosition.y + m_target.position.y) * m_YScrollSpeed);
     }
-}
+}   
