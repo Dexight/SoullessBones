51004c5094d077caf182a3cc31c0cf9daf5b813c
Dexight <92735754+Dexight@users.noreply.github.com>
2022-11-28 00:12:47 +0300

    add spikes

--- SoullessBones/Assets/Scripts/SceneScripts/Entrance.cs
+++ SoullessBones/Assets/Scripts/SceneScripts/Entrance.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class Entrance : MonoBehaviour
 {
     public string entrancePassword; //ѕароль, по которому мы спавнимс€ у конкретного входа в локацию.
+    private Spikes spikes;
+    private void Awake()
+    {
+        spikes = GameObject.FindGameObjectWithTag("Spikes").GetComponent<Spikes>();
+    }
+
+    private void Update()
+    {
+        if (spikes.teleport)
+        {
+            if (GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().scenePassword == entrancePassword)
+            {
+                //Debug.Log("Spikes worked");
+                GameManager.instance.Player.transform.position = transform.position;//наша точка спавна
+                spikes.teleport = false;
+            }
+        }
+    }
 
     void Start()
     {
         if(GameManager.instance)
-            if(GameManager.instance.scenePassword == entrancePassword || GameManager.instance.scenePassword == "after_development")
+            if(GameManager.instance.scenePassword == entrancePassword)
             {
                 GameManager.instance.Player.transform.position = transform.position;//наша точка спавна
             }
