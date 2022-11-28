f44a5d395851fee6d550b3c9cba81b1d46a028dd
DmytryPan <92853484+DmytryPan@users.noreply.github.com>
2022-11-28 22:51:26 +0300

    add worked cheatPanel

--- SoullessBones/Assets/Scripts/CheatPanel.cs
+++ SoullessBones/Assets/Scripts/CheatPanel.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class CheatPanel : MonoBehaviour
 {
+    [SerializeField] GameObject Player;
+    [Header("CheatStats")]
+    public bool godMod;
+    public int damage = 5;
+    public bool enableDoubleJumping;
+    public bool enableWallJumping;
+    public bool enableAstral;
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+    }
     void Update()
     {
-        
+        godMod = Player.GetComponent<HealthSystem>().godMod;
+        damage = Player.GetComponent<AttackSystem>().SlashLeft.GetComponent<Slash>().damage;
+        enableDoubleJumping = Player.GetComponent<DoubleJumping>().enabled;
+        enableWallJumping = Player.GetComponent<WallJumping>().enabled;
+        enableAstral = Player.GetComponent<Astral>().enabled;
     }
 }
