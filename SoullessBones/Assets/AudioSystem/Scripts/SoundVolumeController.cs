51f12b12553f98574ccaca3380ed2b97ed534860
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2023-03-11 16:32:48 +0300

    add menu music volume settings

--- SoullessBones/Assets/AudioSystem/Scripts/SoundVolumeController.cs
+++ SoullessBones/Assets/AudioSystem/Scripts/SoundVolumeController.cs
@@ -5,61 +5,71 @@ public class SoundVolumeController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private Slider slider;
-    [SerializeField] private Text text;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider effectSlider;
 
     [Header("Keys")]
-    [SerializeField] private string saveVolumeKey;
+    [SerializeField] private string saveMusicVolumeKey;
+    [SerializeField] private string saveEffectVolumeKey;
 
     [Header("Tags")]
-    [SerializeField] private string sliderTag;
-    [SerializeField] private string textVolumeTag;
+    [SerializeField] private string musicSliderTag;
+    [SerializeField] private string effectSliderTag;
     
     [Header("Parameters")]
-    [SerializeField] private float volume;
+    [SerializeField] private float musicVolume;
+    [SerializeField] private float effectVolume;
 
     private void Awake()
     {
-        if(PlayerPrefs.HasKey(saveVolumeKey))
+        InitSliders();
+        if (PlayerPrefs.HasKey(saveMusicVolumeKey) && PlayerPrefs.HasKey(saveEffectVolumeKey))
         {
-            volume = PlayerPrefs.GetFloat(saveVolumeKey);
-            audioSource.volume = volume;
-
-            GameObject sliderObj = GameObject.FindWithTag(sliderTag);
-            if (sliderObj != null)
-            {
-                slider = sliderObj.GetComponent<Slider>();
-                slider.value = volume;
-            }
+            musicVolume = PlayerPrefs.GetFloat(saveMusicVolumeKey);
+            effectVolume = PlayerPrefs.GetFloat(saveEffectVolumeKey);
+            //effectVolumeSet
         }
         else 
         {
-            volume = 0.5f;
-            PlayerPrefs.SetFloat(saveVolumeKey, volume);
-            audioSource.volume = volume;
+            musicVolume = 0.5f;
+            effectVolume = 0.5f;
+            PlayerPrefs.SetFloat(saveMusicVolumeKey, musicVolume);
+            PlayerPrefs.SetFloat(saveEffectVolumeKey, effectVolume);
+            //effectVolumeSet
         }
+        audioSource.volume = musicVolume;
+    }
+    private void InitSliders()
+    {
+        if (GameObject.FindWithTag(musicSliderTag) == null) return;
+
+        musicSlider = GameObject.FindWithTag(musicSliderTag).GetComponent<Slider>();
+        musicSlider.value = musicVolume;
+        musicSlider.onValueChanged.AddListener(musicValueChanged);
+
+        effectSlider = GameObject.FindWithTag(effectSliderTag).GetComponent<Slider>();
+        effectSlider.value = effectVolume;
+        effectSlider.onValueChanged.AddListener(effectValueChanged);
+
     }
 
-    private void LateUpdate()
+    private void musicValueChanged(float a)
     {
-        GameObject sliderObj = GameObject.FindWithTag(sliderTag);
-        if(sliderObj != null)
+        musicVolume = a;
+        PlayerPrefs.SetFloat(saveMusicVolumeKey, musicVolume);
+        audioSource.volume = musicVolume;
+    }
+    private void effectValueChanged(float a)
+    {
+        effectVolume = a;
+        PlayerPrefs.SetFloat(saveEffectVolumeKey, effectVolume);
+        //effectVolumeSet
+    }
+    private void Update()
+    {
+        if (musicSlider == null)
         {
-            slider = sliderObj.GetComponent<Slider>();
-            volume = slider.value;
-            if(audioSource.volume != volume)
-            {
-                PlayerPrefs.SetFloat(saveVolumeKey, volume);
-            }
+            InitSliders();
         }
-
-        GameObject textObg = GameObject.FindWithTag(textVolumeTag);
-        if (textObg != null)
-        {
-            text = textObg.GetComponent<Text>();
-            text.text = Mathf.Round(f:volume * 100) + "%";
-        }
-
-        audioSource.volume = volume;
     }
 }
