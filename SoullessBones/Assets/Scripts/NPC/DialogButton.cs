c4c0093dcf7429da3545d931a5aba48b71f64293
Dexight <tupikin@sfedu.ru>
2023-05-04 12:30:21 +0300

    NPC empty dialogs

--- SoullessBones/Assets/Scripts/NPC/DialogButton.cs
+++ SoullessBones/Assets/Scripts/NPC/DialogButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turtle : MonoBehaviour
+public class DialogButton : MonoBehaviour
 {
     [SerializeField] private GameObject button;
     void Start()
