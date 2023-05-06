360778f036acde6a244282f166ffe142b8210387
Denis <109104246+Denis0014@users.noreply.github.com>
2023-05-06 16:59:07 +0300

    Add Dialogs

--- SoullessBones/Assets/Scripts/OtherScripts/FirstDialog.cs
+++ SoullessBones/Assets/Scripts/OtherScripts/FirstDialog.cs
@@ -6,13 +6,18 @@ public class FirstDialog : MonoBehaviour
 {
     [SerializeField] private GameObject DialogManager;
     [SerializeField] private GameObject dialogItem;
+    [SerializeField] private GameObject elf;
     private void Start()
     {
         if (GameManager.instance.enterPassword != "level_01_00")
         {
             Destroy(dialogItem.GetComponent<StartDialogueScript>());
-            Destroy(DialogManager);
-            Destroy(gameObject);
+            // Destroy(DialogManager);
+            // Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(elf.GetComponent<DialogueScript>());
         }
     }
 }
